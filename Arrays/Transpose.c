298fc812ebeec0d16f2103e6bd0986edbe69e780
pramitbhatia25 <pramitbhatia25@gmail.com>
2020-11-25 16:30:09 +0530

    Edits

--- Arrays/Transpose.c
+++ Arrays/Transpose.c
@@ -4,35 +4,39 @@
 
 int main()
 {
-	int array[3][3];
+	int array[3][4];
 	for(int i= 0; i < 3; ++i)
 	{
-		for(int j = 0; j < 3; ++j)
+		for(int j = 0; j < 4; ++j)
 		{
 			printf("%d %d \n", i,j);
 			scanf("%d", &array[i][j]);
 		}
 	}
 	
-	int arrayt[3][3];
-	for(int i= 0; i < 3; ++i)
+	int arrayt[4][3];
+	for(int i= 0; i < 4; ++i)
 	{
 		for(int j = 0; j < 3; ++j)
 		{
 			arrayt[i][j] = array[j][i];
 		}
 	}
+	
+	printf("Your Matrix:\n");
 
 	for(int i = 0; i < 3; ++i)
 	{
-		for(int j = 0; j < 3; ++j)
+		for(int j = 0; j < 4; ++j)
 		{
 			printf("%d	", array[i][j]);
 		}	
 		printf("\n");
 	}
+
+	printf("Transpose Matrix:\n");
 	
-	for(int i = 0; i < 3; ++i)
+	for(int i = 0; i < 4; ++i)
 	{
 		for(int j = 0; j < 3; ++j)
 		{
